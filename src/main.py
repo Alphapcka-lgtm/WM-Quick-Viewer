4b4e370a670f5ae3e15b4aa5e8cc58da1345dc72
Alphapcka-lgtm <73832788+Alphapcka-lgtm@users.noreply.github.com>
2024-01-12 22:47:10 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,15 +1,16 @@
+from pywmapi.common.enums import Language
 from select_frame import SelectFrame
 from plot_frame import PlotFrame
-import tkinter as tk
 import warframe_market_data
-import pywmapi
+import tkinter as tk
+
 
 def main():
     app = tk.Tk()
     app.title('Warframe.Market App-Dings')
     app.geometry('450x300')
 
-    items_dict = warframe_market_data.get_items_dict(pywmapi.common.enums.Language.de)
+    items_dict = warframe_market_data.get_items_dict(Language.de)
     select_frame = SelectFrame(items_dict, app)
     plot_frame = PlotFrame(app)
     select_frame.register_plot_frame(plot_frame)
