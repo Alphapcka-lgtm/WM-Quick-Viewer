7a6767cdcf82b3cb45541abcda8aa799c195b5d8
Alphapcka-lgtm <73832788+Alphapcka-lgtm@users.noreply.github.com>
2024-05-26 15:56:15 +0200

    removed old experimental stuff

--- src/models/api_requester.py
+++ src/models/api_requester.py
@@ -26,25 +26,3 @@ def request_item_statistics(url_name: str) -> Statistic:
 
 def request_item_orders(url_name: str) -> list[OrderRow]:
     return get_orders(url_name, DEFAULT_PLATFORM)
-
-def request_prime_data() -> dict:
-    # building request url
-    origin = 'https://warframe.fandom.com'
-    path = '/api.php'
-    params = {
-	    'action': "scribunto-console",
-	    'format': "json",
-	    'title': "Module:Void/data",
-	    'content': "",
-	    'question': 'local VoidData = require(\'Module:Void/data\').PrimeData local json = require(\'Module:JSON\') print(json.stringify(VoidData))',
-	    'clear': '1'
-    }
-    query_string = urlencode(params)
-    request_url = f"{origin}{path}?{query_string}"
-
-    # request the actual data
-    response = requests.get(request_url)
-    if response.status_code > 299:
-        raise requests.ConnectionError(f'Invalid Response code: {response.status_code}', response=response)
-    relics_json = json.loads(response.json()['print'])
-    return relics_json
